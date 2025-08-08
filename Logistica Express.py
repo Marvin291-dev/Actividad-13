cfbbe954c7f2f965e2938e81bcefb6e575071b08
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-08-07 19:27:57 -0600

    Quinto commit de Ordenar

--- Logistica Express.py
+++ Logistica Express.py
@@ -18,3 +18,14 @@ class Mensajeria:
                 return
             self.Repartidors.append(Repartidor)
             print("Repartidor agregado correctamente")
+
+    def Ordenar(self):
+        def QuickSort(lista):
+            if len(lista) <= 2:
+                return lista
+            pivote = lista[0]
+            mayores = [x for x in lista[1:] if x.Paquete > pivote.Paquete]
+            iguales = [x for x in lista[1:] if x.Paquete == pivote.Paquete]
+            menores = [x for x in lista[1:] if x.Paquete < pivote.Paquete]
+
+            return QuickSort(mayores) + [pivote] + iguales + QuickSort(menores)
